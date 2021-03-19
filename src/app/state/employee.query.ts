e5ae0f630ae9fa6d9ff92ab9acb80736577248e8
iris dreizenshtok <irisdrei7@gmail.com>
2021-03-19 23:53:06 +0200

    fixing the list in select tag to dynamic list

--- src/app/state/employee.query.ts
+++ src/app/state/employee.query.ts
@@ -1,8 +1,9 @@
 import {Query, QueryEntity} from "@datorama/akita";
 import {EmployeeState, EmployeeStore} from "./employee.store";
-import {combineLatest, Observable} from "rxjs";
+import {combineLatest, from, Observable, of} from "rxjs";
 import {Employee} from "../employee.model";
 import {Injectable} from "@angular/core";
+import {distinct, map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -27,13 +28,29 @@ export class employeeQuery extends QueryEntity<EmployeeState>{
   );
 
   private getFilteredEmployees(filter, employees): Employee[] {
-    let firstFilter = filter.city!='' ? employees.filter(t => t.city == filter.city) : employees;
-    let secondFilter = filter.department!='' ? firstFilter.filter(t => t.department == filter.department): firstFilter;
-    let thirdFilter = filter.firstName !='' ? secondFilter.filter(t => t.firstName == filter.firstName) : secondFilter;
-    let fourthFilter = filter.lastName !='' ? thirdFilter.filter(t => t.lastName == filter.lastName) : thirdFilter;
+    let firstFilter = filter.city!='' ? employees.filter(t => (t.city).toLowerCase() == (filter.city).toLowerCase()) : employees;
+    let secondFilter = filter.department!='' ? firstFilter.filter(t => (t.department).toLowerCase() == (filter.department).toLowerCase()): firstFilter;
+    let thirdFilter = filter.firstName !='' ? secondFilter.filter(t => (t.firstName).toLowerCase() == (filter.firstName).toLowerCase()) : secondFilter;
+    let fourthFilter = filter.lastName !='' ? thirdFilter.filter(t => (t.lastName).toLowerCase() == (filter.lastName).toLowerCase()) : thirdFilter;
 
       return fourthFilter;
     }
+
+  updatedCities$ = this.selectFilteredEmployees$.pipe(map(res => {
+      const cities = res.map(r => r.city); // 1
+      const distinctCities = [...new Set(cities)]; // 2
+      return distinctCities
+    }));
+
+  updatedDepartments$ = this.selectFilteredEmployees$.pipe(map(res => {
+      const departments = res.map(r => r.department); // 1
+      const distinctDepartments = [...new Set(departments)]; // 2
+      return distinctDepartments
+  }));
+
+  updatedIIDs$ = this.selectFilteredEmployees$.pipe(
+    map(employees => employees.map(e => e.id)),
+  );
 
   // getLoaded(): Observable<boolean>{
   //   return this.select(state => state.isLoaded)
